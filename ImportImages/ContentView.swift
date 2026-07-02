3b9f451e97b1cf5ed7b7747aade02d981250e3f0
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-07-02 11:10:19 +0800

    Initial Commit

---
+++ ImportImages/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImportImages
+//
+//  Created by Gabriel Vidal on 2026-07-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImportImages/ImportImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImportImagesApp.swift
+//  ImportImages
+//
+//  Created by Gabriel Vidal on 2026-07-02.
+//
+
+import SwiftUI
+
+@main
+struct ImportImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
